54c5aa7e40585a664c76e737af233949c9b5647c
namelew <71669420+namelew@users.noreply.github.com>
2020-11-18 16:09:52 -0300

    Uoload de Arquivos
    Foi criado o primeiro código usando interface gráfica e um arquivo com a explicação das funções

---
+++ tkinter/functions_tkinter.py
@@ -0,0 +1,13 @@
+# Funções e Classes
+# Usar python -m tkinter no terminal para dar desplay da versão da biblioteca e um
+# exemplo de interface
+import tkinter
+# Class tkinter.Tk():
+# - Classe principal e contem a página inicial
+# - Iniciada sem argumentos
+objeto_tk = tkinter.Tk()
+
+# Funtion tkinter.Tcl():
+# - Substituto do .Tk()
+# - Cria um objeto que "ignora" o Tcl interpreter.
+objecto_tcl = tkinter.Tcl()
