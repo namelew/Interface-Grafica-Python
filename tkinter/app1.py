24bda41488b26ef83c636fd70a3de563347a6cf0
namelew <71669420+namelew@users.noreply.github.com>
2020-11-19 14:52:23 -0300

    primeira aula do curso de tkinter
    primeiro programa e primeiras funções

---
+++ tkinter/app1.py
@@ -0,0 +1,5 @@
+import tkinter
+
+menu_inicial = tkinter.Tk()
+menu_inicial.title("Primeira app") # altera o título da página
+menu_inicial.mainloop() # abre a janela principal
--- tkinter/functions_tkinter.py
+++ tkinter/functions_tkinter.py
@@ -6,6 +6,12 @@ import tkinter
 # - Classe principal e contem a página inicial
 # - Iniciada sem argumentos
 objeto_tk = tkinter.Tk()
+# Abre a página
+objeto_tk.mainloop()
+# altera o título da página objeto_tk
+objeto_tk.title("Título")
+
+
 
 # Funtion tkinter.Tcl():
 # - Substituto do .Tk()
